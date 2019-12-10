103bcb09949af53b7d1632e55e624dcd15f3c07f
thezbrooke <37086579+thezbrooke@users.noreply.github.com>
2019-12-09 19:24:06 -0500

    Fixed

---
+++ lab-vr/Assets/eatHay (2019_10_26 18_29_56 UTC).cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class eatHay : MonoBehaviour
+{
+public Image customPanel;
+public Image defaultPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+void OnTriggerEnter(Collider other) 
+    {
+     if (other.gameObject.CompareTag("eat"));
+         {
+            Debug.Log("Compared tag");
+            defaultPanel.enabled = false;
+            Debug.Log("Default off");
+            customPanel.enabled = true;
+            Debug.Log("Win on");
+
+            }
+    }
+
+}
